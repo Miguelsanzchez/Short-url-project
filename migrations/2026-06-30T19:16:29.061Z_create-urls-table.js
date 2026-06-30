c4951938bab5b3c26fb64945b91cb8257199355d
Miguel Sanz <miguelsanzchez94@gmail.com>
2026-06-30 23:08:19 +0200

    chore: added create url table migration

---
+++ migrations/2026-06-30T19:16:29.061Z_create-urls-table.js
@@ -0,0 +1,21 @@
+const { sql } = require("slonik");
+ 
+ 
+ const up = sql.unsafe`
+ CREATE TABLE urls (
+ id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+ origin VARCHAR(150) NOT NULL,
+ destination VARCHAR (30) NOT NULL,
+ created_at TIMESTAMP DEFAULT NOW(),
+ update_at TIMESTAMP DEFAULT NOW()
+);
+`;
+ 
+ const down = sql.unsafe`
+ DROP TABLE urls;
+ `;
+ 
+ module.exports = {
+     up,
+     down,
+};
